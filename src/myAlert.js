997080b6fe8dc61fad53469aa1e27f31b402d3c1
lequangtoi202 <2051052140toi@ou.edu.vn>
2023-05-15 08:04:19 +0700

    Css for default layout

--- src/myAlert.js
+++
@@ -1,5 +0,0 @@
-const myAlert = () => {
-   alert("hi")
-}
-
-export default myAlert;
---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+//Pages
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+//layouts
+import { HeaderOnly } from '~/components/layout';
+
+//Public routes
+const publicRoutes = [
+  { path: '/', component: Home },
+  { path: '/following', component: Following },
+  { path: '/profile', component: Profile },
+  { path: '/search', component: Search, layout: null },
+  { path: '/upload', component: Upload, layout: HeaderOnly },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
