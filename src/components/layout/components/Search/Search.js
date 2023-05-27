1be5f90f1bb057a7d163ae4691ac4c9d5b2b9b2f
lequangtoi202 <2051052140toi@ou.edu.vn>
2023-05-27 08:41:43 +0700

    Use Axios

--- src/components/layout/components/Search/Search.js
+++ src/components/layout/components/Search/Search.js
@@ -4,10 +4,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import HeadlessTippy from '@tippyjs/react/headless';
 import classNames from 'classnames/bind';
 
+import * as searchServices from '~/apiServices/searchServices';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import { AccountItem } from '../../AccountItem';
 import { SearchIcon } from '~/components/Icons';
-// import { useDebounce } from '~/hooks';
+import { useDebounce } from '~/hooks';
 import styles from './Search.module.scss';
 
 const cx = classNames.bind(styles);
@@ -18,42 +19,26 @@ function Search() {
   const [showResult, setShowResult] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  // const debouncedValue = useDebounce(searchValue, 500);
+  const debouncedValue = useDebounce(searchValue, 500);
 
   const inputRef = useRef();
   useEffect(() => {
-    if (!searchValue.trim()) {
+    if (!debouncedValue.trim()) {
+      setSearchResult([]);
       return;
     }
-    setLoading(true);
 
-    fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
-      .then((res) => res.json())
-      .then((res) => {
-        setLoading(false);
-        setSearchResult(res.data);
-      })
-      .catch(() => {
-        setLoading(false);
-      });
-  }, [searchValue]);
-  // useEffect(() => {
-  //   if (!debouncedValue.trim()) {
-  //     setSearchResult([]);
-  //     return;
-  //   }
+    const fetchApi = async () => {
+      setLoading(true);
 
-  //   const fetchApi = async () => {
-  //     setLoading(true);
+      const result = await searchServices.search(debouncedValue);
+      setSearchResult(result);
 
-  //     // const result = await searchServices.search(debouncedValue);
+      setLoading(false);
+    };
 
-  //     // setSearchResult(result);
-  //     setLoading(false);
-  //   };
-
-  //   fetchApi();
-  // }, [debouncedValue]);
+    fetchApi();
+  }, [debouncedValue]);
 
   const handleClear = () => {
     setSearchValue('');
